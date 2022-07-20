b8631c55f9780b040ed834c8c4e19eedc3ac35fd
Jahid Hasan Rony <61205383+jahidrony037@users.noreply.github.com>
2022-07-20 18:56:38 +0600

    car services server gitignore updated

--- index.js
+++ index.js
@@ -14,8 +14,6 @@ app.use(express.json());
 
 //geniuscarservices database
 
-//username: geniuscarservices
-//password: iny8009I3rSJ8hIZ
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.zrwofka.mongodb.net/?retryWrites=true&w=majority`;
